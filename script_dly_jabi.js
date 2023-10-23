ccc71d73cdcf2cb3b1a8cdd1d349c725a4075a3c
Dobromilov <dobromilovartem@gmail.com>
2023-10-23 15:22:41 +0700

    сделал файл преоброзование массива 2.13

--- script_dly_jabi.js
+++ script_dly_jabi.js
@@ -1,89 +1,35 @@
-
-// ЗАДАЧА 1
-            // let array1 = [1,1,2,3,5,8,13,21,35]
-            // let summa=0
-            // let summa_2=0
-            // let composition=1
-            // let sum_6=0
-            // let average=0
-            // let odd=0
-            // for (let i=0; i<array1.length;i++){
-            //     summa+=array1[i]
-            //     composition*=array1[i]
-            //     summa_2+=array1[i]**2
-            //     if (i<6){
-            //         sum_6+=array1[i]
-            //     }
-            //     if (i%2!=0){
-            //         odd+=array1[i]
-            //     }
-            // }
-            // average=summa/array1.length
-            // console.log(summa)
-            // console.log(composition)
-            // console.log(summa_2)
-            // console.log(sum_6)
-            // console.log(average)
-            // console.log(odd)
-// Определить:
-// а)  сумму всех элементов массива;
-// б)  произведение всех элементов массива;
-// в)  сумму квадратов всех элементов массива;
-// г)  сумму шести первых элементов массива;
-// е)  среднее арифметическое всех элементов массива;
-// ё)  сумму нечетных элементов
+// let  mas=['Балбес','Дебил','Дурак']
+// mas.push('свинья')
+// mas.shift()
+// console.log(mas)
 
 
-// ЗАДАЧА 2 
-// Выяснить, верно ли, что сумма элементов массива есть неотрицательное число.
-            // let array2 = [-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1]
-            // let summa=0
-            // for(i=0; i<array2.length; i++){
-            //     summa+=array2[i]
-            // }
-            // if (summa>0){
-            //     console.log('число не отрицательное')
-            // } else  if(summa<0){
-            //     console.log('число отрицательное')
-            // } else if(summa==0){
-            //     ('число равно 0')
-            // }
 
-    
-// ЗАДАЧА 3
-// Найти сумму элементов массива: значение которых не превышает 20
-            // let summa=0
-            // let array3 = [1, 2, 3, 11, 21, 121, 21, 19,-10, 5, 99]
-            // for(i=0; i<array3.length; i++){
-            //     if(array3[i]<=20){
-            //         summa+=array3[i]
-            //     } 
-            // }
-            // console.log(summa)
 
-// ЗАДАЧА 4
-            // В  массиве записаны оценки ученика по 15 предметам. Определить общее количество четверок и пятерок.
-            // let array4 = [3, 3, 3, 4, 3, 5, 5, 4, 3, 4, 5, 4, 3, 5, 5]
-            // let count=0
-            // for(i=0; i<array4.length; i++){
-            //     if((array4[i]==4) || (array4[i]==5)){
-            //         count++
-            //     } 
-            // }
-            // console.log(count)
+// let str='32, 31, 34, 36, 31'
+// let mas=str.split(', ')
+// let line=mas.join(' ; ')
+// console.log(line)
 
-// ЗАДАЧА 5
-// Определить количество положительных и  отрицательных элементов массива.
-            // let array5 = [-1, 1,-1, 1,-1, 1,-1, 1,-1, 1,-1]
-            // let count_pos=0
-            // let count_neg=0
-            // for(i=0; i<array5.length; i++){
-            //     if (array5[i]>0){
-            //         count_pos+=1
-            //     }
-            //     if (array5[i]<0){
-            //         count_neg+=1
-            //     }
-            // }
-            // console.log(count_pos)
-            // console.log(count_neg)
+
+
+// const mas=[1,2,3,4,5,6,7,8,9,10]
+// const [first,second,...other]=mas
+// console.log(first)
+// console.log(second)
+// console.log(other)
+
+
+
+// const mas=[1,2,3,4,5,6,7,8,9,10]
+// console.log(Math.max(...mas))
+
+
+
+
+// const mas=[1,2,3,4,5]
+// const array=[...mas.map(function(num){
+//     return num*2
+// })]
+// console.log(mas)
+// console.log(array)
