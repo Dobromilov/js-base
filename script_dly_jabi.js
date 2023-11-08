efe659ee6fb610b7f0d995dd0e6854328f85a9c8
Dobromilov <dobromilovartem@gmail.com>
2023-11-08 15:24:56 +0700

    сделал до стула

--- script_dly_jabi.js
+++ script_dly_jabi.js
@@ -1,35 +1,45 @@
-// let  mas=['Балбес','Дебил','Дурак']
-// mas.push('свинья')
-// mas.shift()
-// console.log(mas)
+//  Создайте объект user, с полями ваше имя, 
+// пол и дату рождения (name, gender, birthday)Выведите в консоль дату рождения
+ 
+let i = {
+    name:'Arteom',
+    gender:'man',
+    birthday: '15.08.06',
+    pincode: ['2','3','4'],
+    admin: {
+        name: 'Dobromilov',
+        id: 1
+        }
+ };
+ i.game = 'dota 2'
+ delete i.gender
 
+//  for (key in player) {
+//     // тело цикла выполняется для каждого свойства объекта
+//     console.log(key) // Вывести ключи
+//     }
+//     // name isPremium level status
 
+for (key in i) {
+    console.log(i[key]) 
+    }
+    const playerClone1 = {...i} //...- это epread оператор
+    console.log(playerClone1)
 
+    /* {
+    name: "Юля Варкрафтовна"
+    isPremium: true
+    level: 14
+    } */
 
-// let str='32, 31, 34, 36, 31'
-// let mas=str.split(', ')
-// let line=mas.join(' ; ')
-// console.log(line)
-
-
-
-// const mas=[1,2,3,4,5,6,7,8,9,10]
-// const [first,second,...other]=mas
-// console.log(first)
-// console.log(second)
-// console.log(other)
-
-
-
-// const mas=[1,2,3,4,5,6,7,8,9,10]
-// console.log(Math.max(...mas))
-
-
-
-
-// const mas=[1,2,3,4,5]
-// const array=[...mas.map(function(num){
-//     return num*2
-// })]
-// console.log(mas)
-// console.log(array)
+    const player = {
+        name: "Юля Варкрафтовна",
+        isPremium: true,
+        level: 14,
+        }
+        const playerClone = {
+        ...player,
+        name: 'Просто Василий',
+        age: 20,
+        }
+        console.log(playerClone)
