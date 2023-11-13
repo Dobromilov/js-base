131e878db671be791892d38c05f14e6b128dbfba
Dobromilov <dobromilovartem@gmail.com>
2023-11-13 15:28:49 +0700

    страх всего всего

--- script_dly_jabi.js
+++ script_dly_jabi.js
@@ -1,45 +1,57 @@
-//  Создайте объект user, с полями ваше имя, 
-// пол и дату рождения (name, gender, birthday)Выведите в консоль дату рождения
- 
-let i = {
-    name:'Arteom',
-    gender:'man',
-    birthday: '15.08.06',
-    pincode: ['2','3','4'],
-    admin: {
-        name: 'Dobromilov',
-        id: 1
-        }
- };
- i.game = 'dota 2'
- delete i.gender
+// Получаем объект body
+const bodyElement = document.body
 
-//  for (key in player) {
-//     // тело цикла выполняется для каждого свойства объекта
-//     console.log(key) // Вывести ключи
-//     }
-//     // name isPremium level status
+// Первый и последний дочерние элементы
+const firstChildNode = bodyElement.firstChild
+const lastChildNode = bodyElement.lastChild
 
-for (key in i) {
-    console.log(i[key]) 
-    }
-    const playerClone1 = {...i} //...- это epread оператор
-    console.log(playerClone1)
+console.log(firstChildNode)
+console.log(lastChildNode)
 
-    /* {
-    name: "Юля Варкрафтовна"
-    isPremium: true
-    level: 14
-    } */
+// Коллекция childNodes содержит список всех детей, 
+// включая текстовые узлы
+const childNodes = bodyElement.childNodes
+console.log(childNodes)
 
-    const player = {
-        name: "Юля Варкрафтовна",
-        isPremium: true,
-        level: 14,
-        }
-        const playerClone = {
-        ...player,
-        name: 'Просто Василий',
-        age: 20,
-        }
-        console.log(playerClone)
+
+// Перебор - коллекции
+for (let node of childNodes) {  //перебот каждой переменной в коллекции
+	console.log(node)           // покажет все узлы из коллекции
+}
+/*
+	Если мы сохраним ссылку на body.childNodes и добавим/удалим узлы в DOM, 
+	то они появятся в сохранённой коллекции автоматически.
+*/
+
+/*
+	DOM-коллекции и навигационные свойства, перечисленные далее, 
+  доступны только для чтения.
+	Мы не можем заменить один дочерний узел на другой, 
+  просто написав childNodes[i] = 
+	Для изменения DOM требуются другие методы.
+*/
+// Соседние и родительский узлы
+
+const previousSiblingNode = bodyElement.previousSibling // предыдущий родственик
+const nextSiblingNode = bodyElement.nextSibling // следующий родственик
+const parentNode = bodyElement.parentNode // родитель
+
+// Текстовый узел, т.к. перед body есть перевод строки
+console.log(previousSiblingNode) 
+// null т.к. после body ничего нет
+console.log(nextSiblingNode)
+// весь html документ, потому что это родительский узел
+console.log(parentNode)
+
+// Получаем коллекцию всех дочерних узлов
+const childNodes1 = bodyElement.childNodes
+console.log(childNodes1)
+
+// Получаем коллекцию всех дочерних элементов
+const bodyChildren = bodyElement.children
+console.log(bodyChildren)
+/*
+	В документе, возможено, есть ещё «какой-то HTML-код», 
+	но на момент выполнения скрипта браузер ещё до него не дошёл.
+	Поэтому следует размещать скрипт в самом конце документа
+*/
